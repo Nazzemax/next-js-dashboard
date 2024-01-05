63bba42c8dcfad6efbb7bab4697688b09e432c5a
Nazzemax <hvan1998@inbox.ru>
2024-01-05 22:22:48 +0600

    feat(layout): created pages to display data

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,14 @@
-export default function Customers() {
-    return <p>Customers page</p>
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
+export default async function Customers({
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers('')
+    return <CustomersTable query={query} custome={customers} />
 }
