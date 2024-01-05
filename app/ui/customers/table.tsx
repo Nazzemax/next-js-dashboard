34b0648b53bf01964537ff0372a35f5cb6cb6969
Nazzemax <hvan1998@inbox.ru>
2024-01-05 22:25:59 +0600

    feat(layout): implemented crud functionality added ability to edit and form validation pagination works as well Navbar is fully functioning

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -5,12 +5,16 @@ import {
   CustomersTableType,
   FormattedCustomersTable,
 } from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export default async function CustomersTable({
-  customers,
+  custome,
+  query,
 }: {
-  customers: FormattedCustomersTable[];
+  custome: FormattedCustomersTable[],
+  query:string
 }) {
+  const customers = await fetchFilteredCustomers(query)
   return (
     <div className="w-full">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
